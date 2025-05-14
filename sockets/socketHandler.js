4d577da3063ff297b8f555eeb4c0eeb8b721a62c
ibrahim Taha <1ibrahim1mohamed1@gmail.com>
2025-05-14 18:02:18 +0300

    Update user leave all groups chat rooms

--- sockets/socketHandler.js
+++ sockets/socketHandler.js
@@ -1,69 +1,92 @@
-const socket = require('./socket');
+const socket = require("./socket");
 
-const liveSessionController = require('../controllers/liveSessionController');
+const liveSessionController = require("../controllers/liveSessionController");
 
 module.exports = (io) => {
-	io.on('connection', socket => {
-		console.log(`${socket.id} connected`);
+    io.on("connection", (socket) => {
+        console.log(`${socket.id} connected`);
 
-		// join chat groups room take array of groups IDs
-		socket.on('joinGroupsRooms', (groups) => {
-			socket.join(groups.groupsId);
-			console.log(groups.groupsId);
-		})
-		// general room joining. 
-		socket.on('joinRoom', (key) => {
-			socket.join(key);
-			console.log(key + " joined room");
-		})
+        // join chat groups room take array of groups IDs
+        socket.on("joinGroupsRooms", (data) => {
+            socket.join(data.groupsId);
+            console.log(data.groupsId);
+        });
+        // general room joining.
+        socket.on("joinRoom", (key) => {
+            socket.join(key);
+            console.log(key + " joined room");
+        });
 
-		socket.on('leaveRoom', (groupId) => {
-			socket.leave(groupId);
-			io.to(groupId).emit('user-leaved-group-chat' , {})
-			console.log(`${socket.id} leaved room: ${groupId}`);
-		})
+        socket.on("leaveGroupsRooms", (data) => {
+            socket.leave(data.groupsId);
+            io.to(data.groupsId).emit("user-leaved-group-chat", {
+                message: "user leaved the group chat",
+            });
+            console.log(`${socket.id} leaved room: ${data.groupsId}`);
+        });
 
-		/////////////////////////live session///////////////////////
+        /////////////////////////live session///////////////////////
 
-		socket.on('join-session-room', async ({ sessionId, userId }) => {
-			socket.join(sessionId);
-			await liveSessionController.joinLiveSession(sessionId, userId, socket, io); //include emit ->  user-connected , existing-users
-		})
+        socket.on("join-session-room", async ({ sessionId, userId }) => {
+            socket.join(sessionId);
+            await liveSessionController.joinLiveSession(
+                sessionId,
+                userId,
+                socket,
+                io
+            ); //include emit ->  user-connected , existing-users
+        });
 
-		socket.on('media-control', data => {
-			socket.to(data.sessionId).emit('media-control', data);
-		})
+        socket.on("media-control", (data) => {
+            socket.to(data.sessionId).emit("media-control", data);
+        });
 
-		socket.on('offer', data => {
-			io.to(data.sessionId).emit('offer', data);
-		})
+        socket.on("offer", (data) => {
+            io.to(data.sessionId).emit("offer", data);
+        });
 
-		socket.on('ice-candidate', data => {
-			socket.to(data.sessionId).emit('ice-candidate', data);
-		})
+        socket.on("ice-candidate", (data) => {
+            socket.to(data.sessionId).emit("ice-candidate", data);
+        });
 
-		socket.on('answer', data => {
-			io.to(data.sessionId).emit('answer', data);
-		})
+        socket.on("answer", (data) => {
+            io.to(data.sessionId).emit("answer", data);
+        });
 
-		socket.on('end-live-session', async ({ sessionId, userId, role }) => {
-			socket.leave(sessionId);
-			await liveSessionController.leaveLiveSession(sessionId, userId, socket, 'leave', role, io);
-		})
+        socket.on("end-live-session", async ({ sessionId, userId, role }) => {
+            socket.leave(sessionId);
+            await liveSessionController.leaveLiveSession(
+                sessionId,
+                userId,
+                socket,
+                "leave",
+                role,
+                io
+            );
+        });
 
-		// [for instructor] end session and convert it to the summary button 
-		socket.on('instructor-end-liveSession', async ({ sessionId, userId }) => {
-			socket.leave(sessionId);
-			await liveSessionController.leaveLiveSession(sessionId, userId, socket, 'instructor-end-liveSession', '', io);
-		})
-		///////////////////////
+        // [for instructor] end session and convert it to the summary button
+        socket.on(
+            "instructor-end-liveSession",
+            async ({ sessionId, userId }) => {
+                socket.leave(sessionId);
+                await liveSessionController.leaveLiveSession(
+                    sessionId,
+                    userId,
+                    socket,
+                    "instructor-end-liveSession",
+                    "",
+                    io
+                );
+            }
+        );
+        ///////////////////////
 
-
-		socket.on('disconnect', () => {
-			socket.rooms.forEach(room => {
-				socket.leave(room);
-			});
-			console.log(`${socket.id} disconnected and left all rooms`);
-		})
-	})
-}
+        socket.on("disconnect", () => {
+            socket.rooms.forEach((room) => {
+                socket.leave(room);
+            });
+            console.log(`${socket.id} disconnected and left all rooms`);
+        });
+    });
+};
